d87b2e664cdf21434d7188cc4098a8127a948dbc
qhoeriltila <qhoeriltila@gmail.com>
2025-01-15 02:05:12 +0000

    push yang ke dua

--- lib/app/modules/dashboard/controllers/dashboard_controller.dart
+++ lib/app/modules/dashboard/controllers/dashboard_controller.dart
@@ -1,14 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:myapp/app/data/event_response.dart';
 import 'package:myapp/app/modules/dashboard/views/index_view.dart';
 import 'package:myapp/app/modules/dashboard/views/profile_view.dart';
 import 'package:myapp/app/modules/dashboard/views/your_event_view.dart';
+import 'package:myapp/app/utils/api.dart';
 
 class DashboardController extends GetxController {
+  final _getConnect = GetConnect();
+
+ 
   var selectedIndex = 0.obs;
 
   void changeIndex(int index) {
+    
     selectedIndex.value = index;
+  }
+
+  final token = GetStorage().read('token');
+
+  Future<EventResponse> getEvent() async {
+    final response = await _getConnect.get(
+      BaseUrl.events,
+      headers: {'Authorization': "Bearer $token"},
+      contentType: "application/json",
+    );
+    return EventResponse.fromJson(response.body);
+  }
+  
+  var yourEvents = <Events>[].obs;
+
+  Future<void> getYourEvent() async {
+    final response = await _getConnect.get(
+      BaseUrl.yourEvent,
+      headers: {'Authorization': "Bearer $token"},
+      contentType: "application/json",
+    );
+    final eventResponse = EventResponse.fromJson(response.body);
+    yourEvents.value = eventResponse.events ?? [];
   }
 
   final List<Widget> pages = [
@@ -19,6 +49,8 @@ class DashboardController extends GetxController {
 
   @override
   void onInit() {
+    getEvent();
+    getYourEvent();
     super.onInit();
   }
 
